11f5f7d7bd76b2002f0adf83fa4bc31ddd057e37
j5k0 <git.j5k0@gmai.com>
2026-04-19 01:48:49 +0300

    added file reading

--- IP/Program.cs
+++ IP/Program.cs
@@ -1,7 +1,56 @@
-﻿namespace IP 
+﻿using System.Globalization;
+
+namespace IP 
 {
+    class Place
+    {
+        public int Num { get; set; }
+        public string Name { get; set; }
+        public string UniqueIdentifier { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Place(int Num, string Name, string UniqueIdentifier, double X, double Y)
+        {
+            this.Num = Num;
+            this.Name = Name;
+            this.UniqueIdentifier = UniqueIdentifier;
+            this.X = X;
+            this.Y = Y;
+        }
+    }
     class Program
     {
-
+        static List<Place> ReadFile(string filePath)
+        {
+            List<Place> array = new List<Place>();
+            using(StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while((line = reader.ReadLine()!) != null)
+                {
+                    string[] parts = line.Split(';');
+                    if(parts.Length == 5)
+                    {
+                        int num = int.Parse(parts[0]);
+                        string name = parts[1];
+                        string uniqueIdentifier = parts[2];
+                        double x = float.Parse(parts[3], CultureInfo.InvariantCulture);
+                        double y = float.Parse(parts[4], CultureInfo.InvariantCulture);
+                        Place place = new Place(num, name, uniqueIdentifier, x, y);
+                        array.Add(place);
+                    }
+                }
+                return array;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string filePath = "punktai.csv";
+            List<Place> places = ReadFile(filePath);
+            foreach(Place place in places)
+            {
+                Console.WriteLine($"Num: {place.Num}, Name: {place.Name}, X: {place.X}, Y: {place.Y}");
+            }
+        }
     }
 }
